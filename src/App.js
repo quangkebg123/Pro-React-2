a6ce12938f6b9bb110226d52945483f11074eae7
Nguyen Manh Quang <quangkebg123@gmail.com>
2019-10-27 21:00:42 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      Quang Ke kkakakwewe
-    </div>
-  );
+import React, { Component } from "react";
+import { SportsStoreDataStore } from "./data/DataStore";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch, Redirect }
+  from "react-router-dom";
+import { ShopConnector } from "./shop/ShopConnector";
+export default class App extends Component {
+  render() {
+    return <Provider store={SportsStoreDataStore}>
+      	<Router>
+       	 	<Switch>
+          		<Route path="/shop" component={ShopConnector} />
+          		<Redirect to="/shop" />
+        	</Switch>
+      	</Router>
+    </Provider>
+  }
 }
-
-export default App;
---
+++ src/shop/CategoryNavigation.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { ToggleLink } from "../ToggleLink";
+export class CategoryNavigation extends Component {
+    render() {
+        return <React.Fragment>
+            <ToggleLink to={ this.props.baseUrl } exact={ true }>All</ToggleLink>   
+            {this.props.categories && this.props.categories.map(cat =>
+                <ToggleLink className="btn btn-secondary btn-block" key={cat}
+                    to={`${this.props.baseUrl}/${cat.toLowerCase()}`}>
+                    {cat}
+                </ToggleLink>
+            )}
+        </React.Fragment>
+    }
+}
