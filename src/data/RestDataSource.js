8a21f3c68c1f11c538c541f0cb79d593bc77ded7
Nguyen Manh Quang <quangkebg123@gmail.com>
2019-11-03 16:11:11 +0700

    a

--- src/data/RestDataSource.js
+++ src/data/RestDataSource.js
@@ -7,8 +7,11 @@ export class RestDataSource {
         this.error_handler = err_handler || (() => {});
     }
 
-    GetData = (dataType, params) =>
-        this.SendRequest("get", RestUrls[dataType], params);
+    GetData = (dataType, params) =>{
+        console.log(params,'params');
+        return  this.SendRequest("get", RestUrls[dataType], params);
+    };
+     
 
     StoreData = (dataType, data) => 
         this.SendRequest("post", RestUrls[dataType], {}, data);
